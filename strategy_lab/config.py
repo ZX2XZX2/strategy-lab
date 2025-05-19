e7a573f4b28fc26e3fd82c489129cda3703f71d8
ZX2XZX2 <zx2xzx27979@gmail.com>
2025-05-19 01:28:11 -0400

    Added indicators directory

--- strategy_lab/config.py
+++ strategy_lab/config.py
@@ -6,6 +6,7 @@ ROOT_DIR = Path(os.path.join(os.sep, "media", os.getenv("USER"), "SSD", "stx", "
 EOD_DIR = ROOT_DIR / "eod"
 INTRADAY_DIR = ROOT_DIR / "intraday"
 SPLITS_DIR = ROOT_DIR / "splits"
+INDICATORS_DIR = ROOT_DIR / "indicators"
 CALENDAR_PATH = ROOT_DIR / "calendar.parquet"
 
 DB_CONFIG = {
@@ -25,3 +26,5 @@ if not INTRADAY_DIR.exists():
     os.makedirs(INTRADAY_DIR)
 if not SPLITS_DIR.exists():
     os.makedirs(SPLITS_DIR)
+if not INDICATORS_DIR.exists():
+    os.makedirs(INDICATORS_DIR)
